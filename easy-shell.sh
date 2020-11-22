a504ef1f011f0f96ea77508c31bbcc2f1d0da85a
joisaac <joisaac@pm.me>
2020-11-22 09:21:35 -0400

    Easy shell exercises

---
+++ easy-shell.sh
@@ -0,0 +1,11 @@
+# Let's Echo
+echo "HELLO"
+
+# Looping and Skipping
+for n in {1..100..2}; do echo $n; done
+
+# A Personalized Echo
+read name; echo "Welcome $name"
+
+#Looping with numbers
+for n in {1..50}; do echo $n: done
