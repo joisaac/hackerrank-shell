88370f8f64fcb5fc7c9b65b088e42eb808a3f53d
joisaac <joisaac@pm.me>
2020-11-23 09:07:22 -0400

    Easy Shell Exercises

--- easy-shell.sh
+++ easy-shell.sh
@@ -7,5 +7,8 @@ for n in {1..100..2}; do echo $n; done
 # A Personalized Echo
 read name; echo "Welcome $name"
 
-#Looping with numbers
+# Looping with numbers
 for n in {1..50}; do echo $n: done
+
+# The World of Numbers
+read x && read y; for i in {+,-,\*,/}; do echo $(($x $i $y)); done
