f8d5ff3d3ad0bb21c345711e6189011fdb17128b
joisaac <joisaac@pm.me>
2020-12-02 09:01:59 -0400

    Medium Shell Exercises

--- medium-shell.sh
+++ medium-shell.sh
@@ -3,3 +3,15 @@ read i; printf "%.3f\n" $(echo $i | bc -l)
 
 # Compute the Average
 read n; arr=($(cat)); arr=${arr[*]}; printf "%.3f" $(echo $((${arr// /+}))/$n | bc -l)
+
+# Paste -3
+paste -s
+
+# Paste -4
+paste - - -
+
+# Paste -1
+paste -d\; -s
+
+# Paste -2
+paste -d ';' - - -
