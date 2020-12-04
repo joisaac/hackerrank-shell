bdecf1515cc749b07f6b250bd7b57054aa7be283
joisaac <joisaac@pm.me>
2020-12-04 09:48:07 -0400

    Medium Shell Exercises

--- medium-shell.sh
+++ medium-shell.sh
@@ -27,3 +27,12 @@ awk '{s=($2+$3+$4)/3; print $0,":",(s>=80?"A":(s>=60?"B":(s>=50?"C":"FAIL")))}'
 
 # 'Awk' -4
 paste -d';' - -
+
+# 'Grep' #1
+grep -w 'the'
+
+# 'Grep' #2
+grep -iw 'the'
+
+# 'Grep' #3
+grep -wiv "that"
