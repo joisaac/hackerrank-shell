f0fd7772b97b84075a180b73b79ae90556910e3d
joisaac <joisaac@pm.me>
2020-12-04 16:07:21 -0400

    Hard Shell Exercises

---
+++ hard-shell.sh
@@ -0,0 +1,11 @@
+# 'Sed' command #4
+sed 's/[0-9]\+ /**** /g'
+
+# Lonely Integer
+read; echo "$(("$(tr ' ' '^')"))"
+
+# 'Sed' command #4
+sed 's/[0-9]\+ /**** /g'
+
+# 'Sed' command #5
+sed -r 's/(.... )(.... )(.... )(....)/\4 \3\2\1/'
